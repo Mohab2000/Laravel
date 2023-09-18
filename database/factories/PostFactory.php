315167122fa6335e77821ca2d8792af2fc245ab9
Mohab2000 <mohabahmed20005@gmail.com>
2023-09-18 21:50:16 +0300

    Seeding and factories

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $title = fake()->sentence();
+        $slug  = Str::slug($title, '-');
+        return [
+            'title'          => $title,
+            'slug'           => $slug,
+            'excerpt'        => fake()->sentence(),
+            'description'    => fake()->sentence(),
+            'is_published'   => fake()->boolean(),
+            'min_to_read'    => fake()->randomDigit()
+        ];
+    }
+}
