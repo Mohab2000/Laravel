78661cf4d217c4238369db3610c45cdae32c6ca0
Mohab2000 <mohabahmed20005@gmail.com>
2023-09-22 18:48:23 +0300

    all relations added

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,10 +2,42 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Prunable;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes, Prunable;
+    protected $fillable = [
+        "user_id",
+        "title",
+        "slug",
+        "excerpt",
+        "description",
+        "is_published",
+        "min_to_read"
+    ];
+
+    /**
+     * @return Builder
+     */
+    public function prunable(): Builder
+    {
+        return static::where('created_at', '<=', now()->subMonth());
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
+    }
 }
